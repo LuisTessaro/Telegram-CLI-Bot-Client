bbc16d4a53b741c575d262173287b4dc2d471ca4
Luís Henrique Tessaro Pedroso <lh.pedrosot@hotmail.com>
2019-05-14 01:21:25 -0300

    _

--- app/configs/config.js
+++ app/configs/config.js
@@ -1,4 +1,4 @@
 const TelegramBot = require('node-telegram-bot-api')
 const token = ''
 
-module.exports = new TelegramBot(token, { polling: true })
+module.exports = new TelegramBot(token, { polling: true })  
