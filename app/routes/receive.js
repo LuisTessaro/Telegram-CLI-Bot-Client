48e4ad668dc0a727053f38d0a9ae8f9c50a9b6b7
Luis Henrique Tessaro Pedroso <luis.pedroso@lcs.local>
2019-05-15 13:31:19 -0300

    date on print

--- app/routes/receive.js
+++ app/routes/receive.js
@@ -6,16 +6,17 @@ module.exports = (bot) => {
   })
 
   bot.on('message', (msg) => {
+    let d = new Date()
     if (msg.animation)
-      console.log('   ', msg.from.username || 'Anonymous', ':', 'GIF/VIDEO')
+      console.log('   ', d.getHours() + ':' + d.getMinutes(), msg.from.username || 'Anonymous', ':', 'GIF/VIDEO')
 
     if (msg.photo)
-      console.log('   ', msg.from.username || 'Anonymous', ':', 'FOTO')
+      console.log('   ', d.getHours() + ':' + d.getMinutes(), msg.from.username || 'Anonymous', ':', 'FOTO')
 
     if (msg.text)
-      console.log('   ', msg.from.username || 'Anonymous', ':', msg.text)
+      console.log('   ', d.getHours() + ':' + d.getMinutes(), msg.from.username || 'Anonymous', ':', msg.text)
 
     if (msg.sticker)
-      console.log('   ', msg.from.username || 'Anonymous', ':', msg.sticker.emoji, '  Sticker', stickerParser.idToName(msg.sticker.file_id) ? stickerParser.idToName(msg.sticker.file_id) : msg.sticker.file_id)
+      console.log('   ', d.getHours() + ':' + d.getMinutes(), msg.from.username || 'Anonymous', ':', msg.sticker.emoji, '  Sticker', stickerParser.idToName(msg.sticker.file_id) ? stickerParser.idToName(msg.sticker.file_id) : msg.sticker.file_id)
   })
 }
