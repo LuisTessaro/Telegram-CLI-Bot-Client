79104e672446eaf003c9c7a7ee85be938297ee98
Luis Henrique Tessaro Pedroso <luis.pedroso@lcs.local>
2019-05-15 13:34:12 -0300

    better logging

--- app/routes/receive.js
+++ app/routes/receive.js
@@ -7,16 +7,17 @@ module.exports = (bot) => {
 
   bot.on('message', (msg) => {
     let d = new Date()
+    const message = '   ' + d.getHours() + ':' + d.getMinutes() + ' ' + (msg.from.username || 'Anonymous') + ':'
     if (msg.animation)
-      console.log('   ', d.getHours() + ':' + d.getMinutes(), msg.from.username || 'Anonymous', ':', 'GIF/VIDEO')
+      console.log(message, 'GIF/VIDEO')
 
     if (msg.photo)
-      console.log('   ', d.getHours() + ':' + d.getMinutes(), msg.from.username || 'Anonymous', ':', 'FOTO')
+      console.log(message, 'FOTO')
 
     if (msg.text)
-      console.log('   ', d.getHours() + ':' + d.getMinutes(), msg.from.username || 'Anonymous', ':', msg.text)
+      console.log(message, msg.text)
 
     if (msg.sticker)
-      console.log('   ', d.getHours() + ':' + d.getMinutes(), msg.from.username || 'Anonymous', ':', msg.sticker.emoji, '  Sticker', stickerParser.idToName(msg.sticker.file_id) ? stickerParser.idToName(msg.sticker.file_id) : msg.sticker.file_id)
+      console.log(message, msg.sticker.emoji, '  Sticker', stickerParser.idToName(msg.sticker.file_id) ? stickerParser.idToName(msg.sticker.file_id) : msg.sticker.file_id)
   })
 }
